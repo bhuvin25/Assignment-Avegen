edb9b43a0fe0779bb6ccf77afc6995fb75c5a577
bhuvinkothari <bhuvinkothari25@outlook.com>
2023-11-10 10:47:00 +0530

    screen name fix

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { View, Button } from 'react-native';
 
-const HomeScreen = ({ navigation }) => {
+const HomeScreen = ({ navigation }: any) => {
   return (
-    <View>
+    <View style={{ marginHorizontal: 10, marginVertical: 20}}>
       <Button
         title="Win"
-        onPress={() => navigation.navigate('Result', { action: 'Win' })}
+        onPress={() => navigation.navigate('Score', { action: 'Win' })}
       />
       <Button
         title="Lose"
-        onPress={() => navigation.navigate('Result', { action: 'Lose' })}
+        onPress={() => navigation.navigate('Score', { action: 'Lose' })}
       />
     </View>
   );
